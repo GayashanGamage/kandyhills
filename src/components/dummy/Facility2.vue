1b2fc835fb143728216acc0a2d739907c1e28f29
gayashan <gayashan.randimagamage@gmail.com>
2025-05-04 17:27:07 +0530

    facility component mobile optimized

--- src/components/dummy/Facility2.vue
+++ src/components/dummy/Facility2.vue
@@ -1,119 +1,45 @@
 <template>
-  <div class="c-level-one-container">
-    <div class="main-container">
-      <div class="container-head">
-        <h3 class="container-title">Get better experience with us</h3>
-        <p class="container-description">
-          Experience these exceptional services with us and make your stay truly
-          unforgettable !
-        </p>
-      </div>
-      <div class="content-container">
-        <div class="facility-container">
-          <span class="facility-icon material-icons">pool</span>
-          <h4 class="facility-header">indoor swimming pool</h4>
-          <p class="facility-content">
-            Take a refreshing dip in our heated indoor pool, perfect for
-            year-round relaxation
-          </p>
-        </div>
-        <div class="facility-container">
-          <span class="facility-icon material-icons">spa</span>
-          <h4 class="facility-header">Spa and wellness center</h4>
-          <p class="facility-content">
-            Unwind with soothing treatments and therapies at our serene wellness
-            center
-          </p>
-        </div>
-        <div class="facility-container">
-          <span class="facility-icon material-icons">restaurant</span>
-          <h4 class="facility-header">Restaurant</h4>
-          <p class="facility-content">
-            Savor delicious cuisine crafted with fresh ingredients at our onsite
-            restaurant
-          </p>
-        </div>
-        <div class="facility-container">
-          <span class="facility-icon material-icons">restaurant</span>
-          <h4 class="facility-header">Free parking</h4>
-          <p class="facility-content">
-            Enjoy hassle free parking with complementary parking for all our
-            guests
-          </p>
-        </div>
+  <div class="flex flex-col w-full max-w-[1450px] min-w-[200px] mx-auto rounded-2xl p-10 gap-5 bg-[#e7c873]">
+    <div class="flex flex-col gap-4">
+      <h3 class="font-black text-3xl">Get better experience with us</h3>
+      <p class="font-light text-lg">
+        Experience these exceptional services with us and make your stay truly
+        unforgettable !
+      </p>
+    </div>
+    <div class="lg:grid lg:grid-cols-4 md:grid md:grid-cols-2 grid grid-cols-1 gap-10">
+      <div class="flex flex-col sm:gap-4 gap-2 lg:justify-start justify-center lg:items-start items-center" v-for="item in facilityList">
+        <span class="material-icons">{{ item.icon }}</span>
+        <h4 class="font-bold text-lg">{{ item.head }}</h4>
+        <p class="md:w-full w-full max-w-[300px] min-w-[200px] lg:text-start text-center">{{ item.description }}</p>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
 
-<style scoped>
-.main-container {
-  width: 98%;
-  height: 450px;
-  border-radius: 18px;
-  background: #e7c873;
-  margin-inline: auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-.container-title {
-  color: #1a1a1a;
-  text-align: center;
-  font-size: 36px;
-  font-style: normal;
-  font-weight: 500;
-}
-.container-description {
-  color: #1a1a1a;
-  text-align: center;
-  font-family: Roboto;
-  font-size: 15px;
-  font-weight: 300;
-  margin-top: 7px;
-}
-.content-container {
-  display: flex;
-  flex-direction: row;
-  /* border: 1px solid black; */
-}
-.facility-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  margin-top: 40px;
-  margin-inline: 60px;
-}
-.material-icons {
-  font-size: 40px;
-  margin-bottom: 20px;
-}
-.facility-header {
-  color: #1a1a1a;
-  text-align: center;
-  font-family: Roboto;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 500;
-  margin-bottom: 12px;
-}
-.facility-content {
-  display: flex;
-  width: 222.11px;
-  height: 45.024px;
-  flex-direction: column;
-  justify-content: center;
-  flex-shrink: 0;
-  color: #1a1a1a;
-  text-align: center;
-  font-family: Roboto;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 22.512px; /* 190% */
-}
-</style>
+const facilityList = [
+  {
+    icon : 'pool',
+    head : 'indoor swimming pool',
+    description : 'Take a refreshing dip in our heated indoor pool, perfect for year-round relaxation'
+  },
+  {
+    icon : 'spa',
+    head : 'Spa and wellness center',
+    description : 'Unwind with soothing treatments and therapies at our serene wellness center'
+  },
+  {
+    icon : 'restaurant',
+    head : 'Restaurant',
+    description : 'Savor delicious cuisine crafted with fresh ingredients at our onsite restaurant'
+  },
+  {
+    icon : 'restaurant',
+    head : 'Free parking',
+    description : 'Enjoy hassle free parking with complementary parking for all our guests'
+  },
+]
+
+</script>
--- src/views/Home2.vue
+++ src/views/Home2.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="w-full h-auto font-[Roboto] p-2 flex flex-col gap-6">
+    <Facility2></Facility2>
     <QandA2></QandA2>
     <Review2></Review2>
     <footer2></footer2>
   </div>
 </template>
 <script setup>
+import Facility2 from '@/components/dummy/Facility2.vue';
 import Footer2 from '@/components/dummy/Footer2.vue';
 import QandA2 from '@/components/dummy/QandA2.vue';
 import Review2 from '@/components/dummy/Review2.vue';
