740ba030dbfc1c57f5a0acfa00b3b1d10a614554
gayashan <gayashan.randimagamage@gmail.com>
2025-05-05 04:55:23 +0530

    optimize intro part

--- src/views/Home2.vue
+++ src/views/Home2.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="w-full h-auto font-[Roboto] p-2 flex flex-col gap-6">
-    <Facility2></Facility2>
-    <QandA2></QandA2>
-    <Review2></Review2>
-    <footer2></footer2>
+  <div class="">
+    <Header2></Header2>
+    <div class="w-full h-auto font-[Roboto] p-2 flex flex-col gap-6">
+      <Facility2></Facility2>
+      <QandA2></QandA2>
+      <Review2></Review2>
+      <footer2></footer2>
+    </div>
   </div>
 </template>
 <script setup>
 import Facility2 from '@/components/dummy/Facility2.vue';
 import Footer2 from '@/components/dummy/Footer2.vue';
+import Header2 from '@/components/dummy/Header2.vue';
 import QandA2 from '@/components/dummy/QandA2.vue';
 import Review2 from '@/components/dummy/Review2.vue';
 </script>
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,51 +1,11 @@
 <template>
-  <div class="level-one">
-    <p class="name">Gamage</p>
-    <p class="name">Gayashan</p>
-    <button class="action-button" @click="clicked">Click me</button>
+  <div class="flex flex-col gap-10 border w-full h-screen">
+    <div class="w-full h-[200px] border "></div>
+    <div class="w-full h-[200px] border relative">
+      <div class="absolute w-[50%] h-[90%] bg-black z-1"></div>
+      <div class="absolute right-0 left-0 top-0 bottom-0 w-[90%] h-[80%] bg-red-500 z-2"></div>
+      <div class="absolute w-[60%] h-[60%] bg-yellow-500 z-3"></div>
+    </div>
   </div>
 </template>
-
-<script setup>
-const clicked = () => {
-  let a = document.getElementsByClassName("name");
-  console.log(a);
-};
-</script>
-
-<style scoped>
-.level-one {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  flex-direction: column;
-}
-.name {
-  border: 1px solid black;
-  width: 100px;
-  height: 100px;
-  margin-bottom: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 4px;
-}
-.action-button {
-  width: 100px;
-  height: 20px;
-}
-.remove {
-  animation: remove 1s ease-in;
-}
-
-@keyframes remove {
-  start {
-    opacity: 1;
-  }
-  end {
-    opacity: 0;
-  }
-}
-</style>
+<script setup></script>
