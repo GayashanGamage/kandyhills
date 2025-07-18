a5e34c5e1d234e2ab1eab27b035fd7bbe7a0b0f8
gayashan <gayashan.randimagamage@gmail.com>
2025-07-19 03:23:29 +0530

    correct router error

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,12 @@ import Gallery from "@/views/Gallery.vue";
 import Home2 from "@/views/Home2.vue";
 import Review from "@/views/Review.vue";
 import Roompage from "@/views/Roompage.vue";
-import Test from "@/views/test.vue";
-import Test2 from "@/views/test2.vue";
-import Test3 from "@/views/test3.vue";
-import Test4 from "@/views/test4.vue";
-import Test5 from "@/views/test5.vue";
-import Test6 from "@/views/test6.vue";
+// import Test from "@/views/test.vue";
+// import Test2 from "@/views/test2.vue";
+// import Test3 from "@/views/test3.vue";
+// import Test4 from "@/views/test4.vue";
+// import Test5 from "@/views/test5.vue";
+// import Test6 from "@/views/test6.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
@@ -41,46 +41,46 @@ const router = createRouter({
       name: "review",
       component: Review,
     },
-    // {
-    //   path: "/home",
-    //   name: "home",
-    //   component: Home,
-    // },
-    {
-      path: "/test",
-      name: "test",
-      component: Test,
-    },
     {
       path: "/room",
       name: "room",
       component: Roompage,
     },
-    {
-      path: "/test2",
-      name: "test2",
-      component: Test2,
-    },
-    {
-      path: "/test3",
-      name: "test3",
-      component: Test3,
-    },
-    {
-      path: "/test4",
-      name: "test4",
-      component: Test4,
-    },
-    {
-      path: "/test5",
-      name: "test5",
-      component: Test5,
-    },
-    {
-      path: "/test6",
-      name: "test6",
-      component: Test6,
-    },
+    // {
+    //   path: "/home",
+    //   name: "home",
+    //   component: Home,
+    // },
+    // {
+    //   path: "/test",
+    //   name: "test",
+    //   component: Test,
+    // },
+    // {
+    //   path: "/test2",
+    //   name: "test2",
+    //   component: Test2,
+    // },
+    // {
+    //   path: "/test3",
+    //   name: "test3",
+    //   component: Test3,
+    // },
+    // {
+    //   path: "/test4",
+    //   name: "test4",
+    //   component: Test4,
+    // },
+    // {
+    //   path: "/test5",
+    //   name: "test5",
+    //   component: Test5,
+    // },
+    // {
+    //   path: "/test6",
+    //   name: "test6",
+    //   component: Test6,
+    // },
   ],
 });
 
