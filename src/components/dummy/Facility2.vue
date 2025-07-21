f6d98ed75c3fd063691a4e71266e131855fe2df4
gayashan <gayashan.randimagamage@gmail.com>
2025-07-21 18:49:56 +0530

    change facility view

--- src/components/dummy/Facility2.vue
+++ src/components/dummy/Facility2.vue
@@ -12,17 +12,17 @@
     <div
       class="flex flex-col justify-center w-full max-w-[1450px] min-w-[200px] mx-auto rounded-2xl gap-10 text-white relative h-auto">
       <img :src="hotelstore.gallery[24].url" alt=""
-        class="abosolute top-0 left-0 rounded-xl lg:h-[300px] sm:h-[540px] h-[950px] object-cover">
+        class="abosolute top-0 left-0 rounded-xl lg:h-[380px] sm:h-[580px] h-[1100px] object-cover">
       <div class="flex flex-col justify-center items-center absolute top-0 left-0 right-0 bottom-0 p-6">
         <div class="grid lg:grid-cols-4 sm:grid-cols-2 gap-6 w-fit">
 
           <!-- facility list -->
           <div
-            class="flex flex-col justify-center items-center gap-2 bg-black/20 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 p-8 hover:scale-104 duration-100 shadow-lg"
+            class="flex flex-col justify-center items-center gap-4 bg-black/70 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 p-8 hover:scale-104 duration-100 shadow-lg"
             v-for="item in facilityList">
-            <span class="material-icons text-yellow-400">{{ item.icon }}</span>
-            <h4 class="font-bold text-lg md:text-start text-center text-white">{{ item.head }}</h4>
-            <p class="md:w-full w-full max-w-[300px] min-w-[200px] lg:text-center text-center">{{
+            <span class="material-icons text-yellow-400 scale-150">{{ item.icon }}</span>
+            <h4 class="font-bold sm:text-xl text-2xl md:text-start text-center text-white">{{ item.head }}</h4>
+            <p class="md:w-full text-center text-xl font-thin">{{
               item.description
             }}
             </p>
